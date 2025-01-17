ca0824b275d4bb00f426619e1e0a2138b4e261d3
Abel Díaz <diazabel743@gmail.com>
2025-01-17 17:21:31 -0300

    Update settings.py

--- poliroad/settings.py
+++ poliroad/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # que servidores pueden hacer peticiones (cors authorization)
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "poliroadapi.onrender.com"
+    "https://poliroadapi.onrender.com"
 ]
 
 
